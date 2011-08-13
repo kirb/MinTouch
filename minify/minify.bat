dd7b05cdf1ba5f2742ca6ddc2e62f91ac0f4b33e
Adam D <kirbylover4000@gmail.com>
2011-08-13 16:25:44 +0930

    Updated minified script and minify.bat

--- minify/minify.bat
+++ minify/minify.bat
@@ -1,5 +1,5 @@
 @echo off&setlocal
 echo Please Wait, minifying MinTouch...
-cscript /nologo minify.wsf ..\MinTouch.js>..\MinTouch.min.js
+cscript /nologo "%~dp0minify.wsf" "%~dp0..\MinTouch.js">"%~dp0..\MinTouch.min.js"
 echo ...Done, press any key to exit.
 pause>nul
